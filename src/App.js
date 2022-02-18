d21f1217ca2ca39aea28e9934d47e712a27a8154
Yash Rawat <rawatyash1999@gmail.com>
2022-02-18 21:53:31 +0530

    Ref and grid css masonary

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import unsplash from './api/unsplash';
+import ImageList from './ImageList';
 
 import SearchBar from './SearchBar';
 class App extends React.Component {
@@ -19,7 +20,8 @@ class App extends React.Component {
     return (
       <div className='ui container' style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+
+        <ImageList images={this.state.images} />
       </div>
     );
   }
