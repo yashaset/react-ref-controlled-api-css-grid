ca3316b97a08d2df8ded6562fc7826b6d55277f3
Yash Rawat <rawatyash1999@gmail.com>
2022-02-19 22:43:48 +0530

    use ref and event bubbling

--- ooks/src/Dropdown.js
+++ ooks/src/Dropdown.js
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+  useEffect(() => {
+    document.body.addEventListener(
+      'click',
+      (event) => {
+        if (ref.current.contains(event.target)) {
+          return;
+        }
+        setOpen(false);
+      },
+      { capture: true }
+    );
+  }, []);
+
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
       return null;
@@ -17,7 +31,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     );
   });
   return (
-    <div className='ui form'>
+    <div className='ui form' ref={ref}>
       <div className='field'>
         <label className='label'>Select a Color</label>
         <div
