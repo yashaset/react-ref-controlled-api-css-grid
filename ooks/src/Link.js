4960464598b8dfa852800afd5eb81fda56d9ffdd
Yash Rawat <rawatyash1999@gmail.com>
2022-02-20 12:42:11 +0530

    ctrl or cmd click to open in new tab

--- ooks/src/Link.js
+++ ooks/src/Link.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 const Link = ({ href, className, children }) => {
   const onClick = (e) => {
+    if (e.metaKey || e.ctrlKey) {
+      return;
+    }
     e.preventDefault();
     window.history.pushState({}, '', href);
     const navEvent = new PopStateEvent('popState');
